61eaab9fa28f1c52bfbc03cd5c7842a347cded50
Yitao Li <yitao@rstudio.com>
2020-11-05 17:18:35 +0000

    bump r version
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- install_r_on_emr.sh
+++ install_r_on_emr.sh
@@ -2,10 +2,12 @@
 
 set -euf -o pipefail -x
 
+RVERSION='R-4.0.3'
+
 sudo yum -y install gcc-gfortran libgfortran readline-devel libxml2-devel libcurl-devel pcre2-devel bzip2-devel xz-devel
-wget https://cran.r-project.org/src/base/R-4/R-4.0.0.tar.gz
-tar xf R-4.0.0.tar.gz
-cd R-4.0.0
+wget "https://cran.r-project.org/src/base/R-4/${RVERSION}.tar.gz"
+tar xf "${RVERSION}.tar.gz"
+cd ${RVERSION}
 ./configure --prefix=/usr --with-x=no
 make -j4
 sudo make install
