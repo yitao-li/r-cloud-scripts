d01da38a379fc69c386d40126a51cf1fda21314b
Yitao Li <yitao@rstudio.com>
2021-02-04 17:09:26 +0000

    revise install_r_on_hdinsight.sh
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- install_r_on_hdinsight.sh
+++ install_r_on_hdinsight.sh
@@ -2,11 +2,13 @@
 
 set -euf -o pipefail -x
 
+RVERSION='R-4.0.3'
+
 sudo apt-get update
 sudo apt-get install -y gfortran libreadline-dev libxml2-dev libcurl4-openssl-dev libpcre2-dev libbz2-dev liblzma-dev
-wget https://cran.r-project.org/src/base/R-4/R-4.0.0.tar.gz
-tar xf R-4.0.0.tar.gz
-cd R-4.0.0
+wget "https://cran.r-project.org/src/base/R-4/${RVERSION}.tar.gz"
+tar xf "${RVERSION}.tar.gz"
+cd ${RVERSION}
 ./configure --prefix=/usr --with-x=no --enable-R-shlib
 make -j4
 sudo make install
